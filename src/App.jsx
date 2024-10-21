c812c10659f54004350615cfd2c209eee77e4ea1
Azz1fy <azzifyclips@gmail.com>
2024-10-20 22:47:08 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,147 @@
+import React, { useState } from 'react';
+
+export default function App() {
+	const [value, setValue] = useState('');
+
+	function newValue(event) {
+		const clickedValue = event.target.value;
+		setValue((prevValue) => prevValue + clickedValue);
+	}
+
+	return (
+		<body class='min-h-screen bg-[#0F172A]'>
+			<div className='w-96 px-24 py-16'>
+				<form className='' action=''>
+					<div className='content-center'>
+						<input type='text' value={value} />
+					</div>
+
+					<div>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='CE'
+							onClick={(e) => setValue('')}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='X'
+							onClick={(e) => setValue(value.slice(0, -1))}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='.'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='/'
+							onClick={newValue}
+						/>
+					</div>
+					<div>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='7'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='8'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='9'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='*'
+							onClick={newValue}
+						/>
+					</div>
+					<div>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='4'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='5'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='6'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='+'
+							onClick={newValue}
+						/>
+					</div>
+					<div>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='1'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='2'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='3'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='-'
+							onClick={newValue}
+						/>
+					</div>
+					<div>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='00'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='0'
+							onClick={newValue}
+						/>
+						<input
+							className='font-bold m-1 text-white text-2xl bg-[#0EA5E9] rounded-lg box-border h-16 w-16'
+							type='button'
+							value='='
+							onClick={(e) => setValue(eval(value))}
+						/>
+					</div>
+				</form>
+			</div>
+		</body>
+	);
+}
